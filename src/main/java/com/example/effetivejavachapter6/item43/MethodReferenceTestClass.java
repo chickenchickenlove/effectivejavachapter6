edcfc6dcb634ac13f9b9e596236cab8af7431c20
chickenchickenlove <ojt90902@naver.com>
2023-08-08 00:28:27 +0900

    effective java item 43 study

--- src/main/java/com/example/effetivejavachapter6/item43/MethodReferenceTestClass.java
+++ src/main/java/com/example/effetivejavachapter6/item43/MethodReferenceTestClass.java
@@ -2,7 +2,12 @@ package com.example.effetivejavachapter6.item43;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class MethodReferenceTestClass {
 
@@ -12,35 +17,38 @@ public class MethodReferenceTestClass {
         final HashMap<String, Integer> map = new HashMap<>();
         final String key = "hello";
 
+
         // 람다식
-        map.merge(key, 1, (n1, n2, n3, n4, n5, n6) -> n1, n2, n3, n4, n5, n6);
+        map.merge(key, 1, (n1, n2) -> (n1 + n2) * n2 * n1);
 
         // 메서드 참조
         map.merge(key, 1, Integer::sum);
 
-        MethodReferenceTestClass methodReferenceTestClass = new MethodReferenceTestClass();
-        methodReferenceTestClass.myFunction(integer -> );
+        String myString = "hello";
+        myString.toUpperCase();
+
+        // 비한정적 메서드 참조
+        MethodReferenceTestClass r1 = new MethodReferenceTestClass();
+        MethodReferenceTestClass r2 = new MethodReferenceTestClass();
+        MethodReferenceTestClass r3 = new MethodReferenceTestClass();
+        List<MethodReferenceTestClass> myList = List.of(r1, r2, r3);
+
+        myList.stream()
+                .map(MethodReferenceTestClass::getName)
+                .collect(Collectors.toList());
+
+
+        // 한정적 메서드 참조
+        String str = "hello";
+        Supplier<String> supplier = str::toUpperCase;
+
+        // 정적 메서드 참조
+        Function<Double, Double> sqrt = Math::sqrt;
+
 
     }
 
-    public void myFunction(Function<Integer, Integer> func) {
-
-
-
+    public String getName() {
+        return this.toString();
     }
-
-
-    public static int addAllNumber(int ... numbers) {
-        int sum = 0;
-        for (int number : numbers) {
-            sum += number;
-        }
-    }
-
-
-
-
-
-
-
 }
