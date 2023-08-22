08a420e1b538a13d7628c10f5ec3fe6294ea545c
chickenchickenlove <ojt90902@naver.com>
2023-08-22 19:05:23 +0900

    effective java item 58 study

--- src/main/java/com/example/effetivejavachapter6/item58/FrequentMissInFor.java
+++ src/main/java/com/example/effetivejavachapter6/item58/FrequentMissInFor.java
@@ -1,33 +1,74 @@
 package com.example.effetivejavachapter6.item58;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-public class GoodForCase {
+public class FrequentMissInFor {
+
+    enum Suit {CLUB, DIAMOND, HEART, SPADE}
+    enum Rank {ACE, DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}
+    public record Card(Suit suit, Rank rank) {}
 
     public void badCase() {
-        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        int[] intArray = new int[10];
 
-        for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext();) {
-            Integer next = iterator.next();
-        }
+        List<Suit> suits = Arrays.asList(Suit.values());
+        List<Rank> ranks = Arrays.asList(Rank.values());
 
-        for (int i = 0; i < list.size(); i++) {
-            Integer integer = list.get(i);
+        List<Card> deck = new ArrayList<>();
+        for (Iterator<Suit> i = suits.iterator(); i.hasNext(); ) {
+            for (Iterator<Rank> j = ranks.iterator(); j.hasNext(); ) {
+                deck.add(new Card(i.next(), j.next()));
+            }
         }
+    }
 
-        for (int i = 0; i < intArray.length; i++) {
-            int i1 = intArray[i];
-        }
+    public void goodCase() {
 
-        for (int i : list) {
-            System.out.println(i);
-        }
+        List<Suit> suits = Arrays.asList(Suit.values());
+        List<Rank> ranks = Arrays.asList(Rank.values());
 
-        for (int i : intArray) {
-            System.out.println(i);
+        List<Card> deck = new ArrayList<>();
+        for (Suit suit : suits)
+            for (Rank rank : ranks)
+                deck.add(new Card(suit, rank));
+    }
+
+    public void isItGood() {
+        List<Suit> suits = Arrays.asList(Suit.values());
+        List<Rank> ranks1 = Arrays.asList(Rank.values());
+        List<Rank> ranks2 = Arrays.asList(Rank.values());
+        List<Rank> ranks3 = Arrays.asList(Rank.values());
+
+        List<Card> deck = new ArrayList<>();
+        for (int i = 0; i < suits.size(); i++) {
+            for (int j = 0; j < ranks3.size(); j++) {
+                deck.add(new Card(suits.get(i), ranks1.get(j)));
+                deck.add(new Card(suits.get(i), ranks2.get(j)));
+                deck.add(new Card(suits.get(i), ranks3.get(j)));
+            }
         }
+    }
+
+    public void thisIsBetter() {
+        List<Suit> suits = Arrays.asList(Suit.values());
+        List<Rank> ranks1 = Arrays.asList(Rank.values());
+        List<Rank> ranks2 = Arrays.asList(Rank.values());
+        List<Rank> ranks3 = Arrays.asList(Rank.values());
+
+        List<Card> deck = new ArrayList<>();
+        for (Suit suit : suits)
+            for (Rank rank : ranks1)
+                deck.add(new Card(suit, rank));
+
+        for (Suit suit : suits)
+            for (Rank rank : ranks2)
+                deck.add(new Card(suit, rank));
+
+        for (Suit suit : suits)
+            for (Rank rank : ranks3)
+                deck.add(new Card(suit, rank));
     }
 
 }
--- src/main/java/com/example/effetivejavachapter6/item58/GoodForCase.java
+++ src/main/java/com/example/effetivejavachapter6/item58/GoodForCase.java
@@ -1,2 +1,33 @@
-package com.example.effetivejavachapter6.item58;public class GoodForCase {
+package com.example.effetivejavachapter6.item58;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class GoodForCase {
+
+    public void badCase() {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        int[] intArray = new int[10];
+
+        for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext();) {
+            Integer next = iterator.next();
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            Integer integer = list.get(i);
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            int i1 = intArray[i];
+        }
+
+        for (int i : list) {
+            System.out.println(i);
+        }
+
+        for (int i : intArray) {
+            System.out.println(i);
+        }
+    }
+
 }
