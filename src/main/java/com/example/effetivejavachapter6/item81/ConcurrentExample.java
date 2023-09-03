6cceca7e0c3f6404be4ca86386b4b5777fb62832
chickenchickenlove <ojt90902@naver.com>
2023-09-03 20:06:57 +0900

    effective java item81 study

---
+++ src/main/java/com/example/effetivejavachapter6/item81/ConcurrentExample.java
@@ -0,0 +1,40 @@
+package com.example.effetivejavachapter6.item81;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+
+public class ConcurrentExample {
+
+    public static long time(Executor executor,
+                            int concurrency,
+                            Runnable action) throws InterruptedException {
+        CountDownLatch ready = new CountDownLatch(concurrency);
+        CountDownLatch start = new CountDownLatch(1);
+        CountDownLatch done = new CountDownLatch(concurrency);
+
+        for (int i = 0; i < concurrency; i++) {
+            executor.execute(() -> {
+                // 타이머에게 준비를 마쳤음을 알린다.
+                ready.countDown();
+                try {
+                    start.await();
+                    action.run();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    done.countDown();
+                }
+            });
+        }
+
+        ready.await(); // 모든 작업자가 준비될 때까지 기다린다.
+        long startNanos = System.nanoTime();
+        start.countDown(); // 작업자들을 개운다.
+        done.await(); // 모든 작업자가 일을 끝마치기를 기다린다.
+        return System.nanoTime() - startNanos;
+    }
+}
+
+
+
+
